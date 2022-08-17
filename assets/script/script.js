1f37becdb3f7c6622e07ee63235a3b1298d2e7b1
hburnton <hburnton@gmail.com>
2022-08-17 10:20:29 -0700

    rework answer assignment

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,10 +7,7 @@ var timer = document.createElement('p');
 var main = document.createElement('main');
 var question = document.createElement('h2');
 var answerList = document.createElement('ol');
-var choice1 = document.createElement('li');
-var choice2 = document.createElement('li');
-var choice3 = document.createElement('li');
-var choice4 = document.createElement('li');
+var answer = [];
 var timeLeft = 0;
 
 /*Considering structure for questions*/
@@ -22,7 +19,7 @@ var questionList = [
                "Inside the <script> element",
                "In the <footer> element"],
      answer: "Inside the <script> element"},
-
+     
      {question: "What operator is used to assign a value to a declared variable?",
       choices: ["Double-equal (==)", 
                "Colon (:)",
@@ -69,9 +66,10 @@ var questionList = [
 /*
 The below shuffle function is taken from this
 https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array\
-}*/ 
+}*/
+
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+    var currentIndex = array.length,  randomIndex;
   
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
@@ -88,7 +86,7 @@ function shuffle(array) {
     return array;
 }
 
-/*shuffle array so when we pop or shift they will be somewhat random aside from last/first question*/
+/*shuffle array*/
 questionList = shuffle(questionList);
 
 viewHighScore.textContent="Click to View High Scores";
@@ -96,18 +94,27 @@ timer.textContent="Time Remaining: " + timeLeft;
 question.textContent = "This is your first question";
 viewHighScore.setAttribute("href", "#");
 
-choice1.textContent = "I am a test";
-choice2.textContent = "I am a test too";
-choice3.textContent = "I'm not";
-choice4.textContent = "Wait me too!";
-
 body.appendChild(header);
 header.appendChild(viewHighScore);
 header.appendChild(timer);
 body.appendChild(main);
 main.appendChild(question);
+main.appendChild(answerList);
 
-console.log(questionList[2].question)
 
-questionList = shuffle(questionList)
-console.log(questionList[2].question)
+function printQuestion(array){
+    if (array.length == 0){
+        console.log('hi');
+    }else{
+        var oneQuestion = array.pop();
+        question.textContent = oneQuestion.question;
+        oneQuestion.choice = shuffle(oneQuestion.choices)
+        for(let i=0; i < oneQuestion.choices.length; i++){
+            answer[i] = document.createElement('li');
+            answer[i].textContent = oneQuestion.choices[i];
+            answerList.appendChild(answer[i]);
+        }
+    } 
+}
+
+printQuestion(questionList)
