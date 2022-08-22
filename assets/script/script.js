64ae727bd182520608fb7b2b7d2ee599638bf6a0
hburnton <hburnton@gmail.com>
2022-08-22 16:35:54 -0700

    clean up code, add more comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,14 +6,14 @@ var main = document.createElement('main');
 var question = document.createElement('h1');
 var answerList = document.createElement('ol');
 var startButton = document.createElement('button');
-var feedback = document.createElement('p')
+var feedback = document.createElement('p');
 var oneQuestion;
 var timerInterval;
 var askedQuestions = [];
 var answer = [];
 var timeLeft = 60;
 var currentScore = 0;
-var userScores = []
+var userScores = [];
 
 /*Questions are sourced from Unit 03: JavaScript Technical Interview Questions*/
 var questionList = [
@@ -69,7 +69,7 @@ var questionList = [
      {question: "Javascript is a good language",
       choices: ["True", "False"],
       answer: "True",},
-]
+];
 
 /*
 The below shuffle function is taken from this
@@ -100,7 +100,7 @@ viewHighScore.textContent="Click to View High Scores";
 timer.textContent="Time Remaining: " + timeLeft;
 question.textContent = "Welcome to Javascript Quiz! Please click Start Now! to get going. You have 60 seconds once the timer starts.";
 viewHighScore.setAttribute("href", "./scores.html");
-startButton.textContent = "Start Now!"
+startButton.textContent = "Start Now!";
 
 body.appendChild(header);
 header.appendChild(viewHighScore);
@@ -110,12 +110,14 @@ main.setAttribute('class', '');
 main.appendChild(question);
 main.appendChild(startButton);
 main.appendChild(answerList);
-main.appendChild(feedback)
+main.appendChild(feedback);
 
 
 function printQuestion(){
 
-    clearScreen();//Clear the screen initially
+    //Clear the screen initially
+    clearScreen();
+    
 
     //if the question list is empty, end the game
     if (questionList.length == 0){
@@ -124,10 +126,13 @@ function printQuestion(){
         //remove a single question object from questionList array, set h2 text content to a question
         oneQuestion = questionList.pop();
         question.textContent = oneQuestion.question;
+
         askedQuestions.push(oneQuestion);
+        //This is an array for asked questions, it is useless right
+        //but allows tracking of questions should they be reshuffled/modified in future iterations
 
         //shuffle the multiple choice answers for variation
-        oneQuestion.choices = shuffle(oneQuestion.choices)
+        oneQuestion.choices = shuffle(oneQuestion.choices);
 
         //Create n amount of listener answer boxes that will check if the selected answer is correct
         for(let i=0; i < oneQuestion.choices.length; i++){
@@ -137,14 +142,6 @@ function printQuestion(){
             answerList.appendChild(answer[i]); 
         }
     } 
-}
-
-//While the OL Answer List Has Children, remove them, this clears all previous entries and sets question h2 to blank string
-function clearScreen(){
-    while(answerList.hasChildNodes()) {
-        answerList.removeChild(answerList.firstChild);
-        question.textContent = '';
-    }
 }
 
 function isRight(event){
@@ -174,21 +171,40 @@ function startTimer() {
     }, 1000);
 }
 
+//While the OL Answer List Has Children, remove them, this clears all previous entries and sets question h2 to blank string
+function clearScreen(){
+    while(answerList.hasChildNodes()) {
+        answerList.removeChild(answerList.firstChild);
+        question.textContent = '';
+    }
+}
+
 function endGame(){
+    //Stop Timer
     clearInterval(timerInterval);
+
     clearScreen();
     feedback.textContent = '';
+    
+    //Displays message for end, a special message if every question has been drawn
     if (questionList.length == 0){
         question.textContent = "You have answered every question. Your score is: "+ currentScore;
     }else{
         question.textContent = "Time is up! Your score is: " + currentScore;
     }
-    logScore = document.createElement('button');
+
+    //Create input text field for user to enter name
     userName = document.createElement('input');
     userName.setAttribute('type', 'text');
     feedback.appendChild(userName);
+
+    //Create button for user to submit name
+    logScore = document.createElement('button');
     logScore.textContent = "Log My Score";
     main.appendChild(logScore);
+
+    //Add event listener to logScore button that receives username, checks for stored
+    //users and adds or creates JSON string.
     logScore.addEventListener('click', function(){
         var user = {
             name: userName.value.trim(),
@@ -198,7 +214,7 @@ function endGame(){
         var storedUsers = JSON.parse(localStorage.getItem("userScores"))
         if (storedUsers !== null) {
             userScores = storedUsers;
-            console.log(userScores)
+            console.log(userScores);
         }
         userScores.push(user);
         localStorage.setItem("userScores", JSON.stringify(userScores));
@@ -206,7 +222,8 @@ function endGame(){
     })
 }
 
-
+//This adds the event listener to the start Button that will start the timer and print question
+//and remove start button
 startButton.addEventListener("click", function(){
     startTimer();
     printQuestion();
